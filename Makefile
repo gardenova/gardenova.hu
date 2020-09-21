115cff3bf200f9efc8e6d23a2df16d03f83cc8da
Peter <mezeipetister@gmail.com>
2020-09-21 08:03:42 +0200

    ..

---
+++ Makefile
@@ -0,0 +1,13 @@
+
+.PHONY: release, build, dev, serve
+
+release:
+	ng build --prod --outputPath=dist
+
+build:
+	ng build --outputPath=dist
+
+dev:
+	ng serve
+
+serve: dev
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gardenova';
+  public constructor(private titleService: Title) {
+    titleService.setTitle(this.title);
+  }
+
+  title = 'Gardenova - Mindenünk a gyümölcs';
   apples: Apple[] = [
     new Apple("Red Chief Delicious (Jeromine)", "redchief.jpg"),
     new Apple("Golden Reinders", "golden.jpg"),
